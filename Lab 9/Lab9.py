255fba4c2a29eb01971b545173afc501ef6754b8
worf <worf@Tardis>
2018-11-16 14:08:12 -0500

    First commit

---
+++ Lab 9/Lab9.py
@@ -0,0 +1,17 @@
+#===============================================================================
+# Lab 9  COMPSCI 1026 UWO
+# November 16, 2018
+# this exercise, you will write a program that makes use of two dictionaries. The program
+# should read the data in rawdata.txt into a dictionary whose keys are country names and whose
+# values are per capita incomes. Also create another dictionary whose keys are letters A‐ Z and
+# whose values are sets of country names. Convert the country names to all uppercase for both
+# dictionaries.
+# Your program should prompt the user to enter an initial or a country names and then print the
+# corresponding values.
+# * If the user enters a string with only one letter (like A), the program should print all the
+# countries that begin with that letter,
+# * If the user enters a string that contains more than one letter, print the corresponding
+# value for that country if it exists or print “Does not exist” if invalid.
+# * Stop when the user enters quit.
+#===============================================================================
+
